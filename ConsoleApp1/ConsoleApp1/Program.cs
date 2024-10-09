2bf27d611630e8d906ff09730ff0aa6f6e5dbe47
Furkan <73752438+pcx260@users.noreply.github.com>
2024-10-09 11:35:38 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class urun
+{
+    public string name;
+    public double price;
+   public  int categorynum;
+
+   
+}
+
+
+class Program
+{
+    static void show(urun a,urun b)
+    {
+        Console.WriteLine(a.price);
+        Console.WriteLine(a.name);
+        Console.WriteLine(a.categorynum);
+        Console.WriteLine("");
+        Console.WriteLine("");
+        Console.WriteLine("");
+        Console.WriteLine(b.price);
+        Console.WriteLine(b.name);
+        Console.WriteLine(b.categorynum);
+
+    }
+
+    static void Main(string[] args)
+    {
+         
+       //pc
+        urun bilgisayar = new urun();
+        bilgisayar.name = "PC";
+        bilgisayar.price = 500;
+        bilgisayar.categorynum = 0;
+
+        //sehpa
+        urun sehpa = new urun();
+        sehpa.name = "sehpa";
+        sehpa.price = 300;
+        sehpa.categorynum = 1;
+
+
+        show(sehpa,bilgisayar);
+        
+    }    
+}
+
